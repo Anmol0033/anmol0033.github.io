6d08371840c66674eaae04dc72e79e772453e334
Anmol0033 <136916667+Anmol0033@users.noreply.github.com>
2023-10-13 21:25:20 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto-type",{
+    strings:[' Anmol Kamboj!'],
+    typeSpeed: 150,
+    // backspeed: 150,
+    loop : false
+})
+var typed = new Typed(".auto-type-skills",{
+    strings:[' Devloper..', 'Student..'],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop : false,
+    startDelay: 3000
+})
